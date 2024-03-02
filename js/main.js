0b6a515e48fadf96fe470a4d32b81ed4e6c5b341
Jordi <jordiotero15@gmail.com>
2024-03-01 22:01:02 -0300

    Fixing filtros

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
 //Importamos funciones
 import { comprarProductos } from "./cart.js";
 
+export let productosDisponibles;
+
 
 //////Pruebbaaa
 const prueba = document.getElementById("prueba");
 
 const pruebaDos = document.getElementById("prueba__dos");
-// prueba.classList.add("nuevaClase");
 
-// // Quitar una clase del elemento
-// prueba.classList.remove("containerGoogle");
 
 //Traemos el div para la card de productos
 const divProductos = document.getElementById("productos");
@@ -29,14 +28,61 @@ const filterNombre = document.getElementById("filter__nombre");
 //Filtro por precio segun pick en la lista
 const filterPrecio = document.getElementById("filter__precio");
 
-//Mostramos productos Disponibles ... y la exportamos a otros archivos .js
-export let productosDisponibles = JSON.parse(localStorage.getItem("productos"));
-
 //Llamamos al usuario logueado
 let usuarioLogueado = JSON.parse(sessionStorage.getItem("usuario"));
 
+////////////
+//Funciones
+
+//1-Modelo de la carta de los productos
+const generarCartaProducto = (producto) => {
+    const { id, nombre, categorias, precio, img, alt } = producto;
+
+    let card = document.createElement("div");
+    card.className = "card col-xl-4 col-md-6 col-sm-12 cardCategorias";
+    card.innerHTML = `
+         <div class="card col-xl-2 col-md-6 col-sm-12 ">
+             <img src="${img}" class="card-img-top" alt="${alt}">
+             <div class="card-body textoCards">
+                 <h4 class="card-title">${nombre}</h4>
+                 <p class="card-text"> ${categorias} </p>
+                 <p class="card-text">$${precio} </p>
+                 <button id="btnComprar${id}" class="btn btn-primary">Comprar</button>
+             </div>
+         </div>
+        `;
+    const btnComprar = card.querySelector(`#btnComprar${id}`);
+    btnComprar.addEventListener("click", () => comprarProductos(id));
+
+    return card;
+};
+//2-Generamos función para mostrar el modelo y las cartas de productos
+const generarCartasProductos = (productos) => {
+    // Limpiamos el contenido anterior
+    divProductos.innerHTML = "";
+    productos.forEach(producto => {
+        const carta = generarCartaProducto(producto);
+        divProductos.appendChild(carta);
+    });
+}
+
+//Generamos una Funcion para mostrar las cards de los productos utilizando asincronia y fetch
+const generarCardsProductos = async () => {
+    try {
+        // Limpiamos el contenido anterior de divProductos
+        divProductos.innerHTML = "";
+        const response = await fetch("../db/productos.json");
+        productosDisponibles = await response.json();
+        // productosDisponibles = productos;
+        //Llamamos a la funcion para mostrar las cartas de los productos disponibles
+        generarCartasProductos(productosDisponibles);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
 //////////////////////////
-//Creamos Evento Para generar el documento siguiente (Son eventos que comienzan ni bien se abre la pagina)
+//Creamos Evento en el DOM para generar el siguiente documento
 document.addEventListener("DOMContentLoaded", () => {
     //Declaramos la varible de user para poder utilizar el nombre de usuario
     if (usuarioLogueado === null) {
@@ -79,129 +125,119 @@ document.addEventListener("DOMContentLoaded", () => {
         userLogin.appendChild(close);
     }
 
-
-    generarCardsProductos(productosDisponibles);
+    //Llamamos a la funcion para mostrar los productos
+    generarCardsProductos();
 });
-
-/////////////////////////
-//Generamos una Funcion para mostrar las cards de los productos
-export const generarCardsProductos = (productos) => {
-    // Limpiamos el contenido anterior de divProductos
-    divProductos.innerHTML = "";
-    productos.forEach(productos => {
-        //Utilizamos la propiedad de desestructuracion
-        const { id, nombre, categorias, precio, img, alt } = productos;
-
-        //Creamos el formato de la carta
-        let card = document.createElement("div");
-        card.className = "card col-xl-4 col-md-6 col-sm-12 cardCategorias";
-        card.innerHTML = `
-         <div class="card col-xl-2 col-md-6 col-sm-12 ">
-         <img src="${img}" class="card-img-top" alt="${alt}">
-         <div class="card-body textoCards">
-          <h4 class="card-title">${nombre}</h4>
-         <p class="card-text"> ${categorias} </p>
-         <p class="card-text">$${precio} </p>
-         <button id="btnComprar${id}" class="btn btn-primary">Comprar</button>
-        </div>
-        </div>
-        `;
-        //Mostramos las cards
-        divProductos.appendChild(card);
-        // //Debemos declarar el id del elemento para obtener cual es el producto seleccionado por eso el ${productos.id}
-        const btnComprar = document.getElementById(`btnComprar${productos.id}`);
-        //Evento al hacer click y llamamos a la funcion de comprar producto declarada en cart.js
-        btnComprar.addEventListener("click", () => comprarProductos(id));
-
-    });
-
-};
 
 /////////////////////
-// Filter Input
+//FILTROS
+//1-Filter Input
 filterInput.addEventListener("keyup", (e) => {
-    //Seteamos que en productosDisponible filtre el producto con el valor.nombre hacerlo minuscula y que incluya el target value
-    const productosFilter = productosDisponibles.filter((producto) => producto.nombre.toLowerCase().includes(e.target.value));
-
-    //Una vez tengo el nuevo arreglo, sobre escribo para generar la carta
-    productosDisponibles = productosFilter;
-
-    if (e.target.value !== "") {
-        generarCardsProductos(productosFilter);
-    } else {
-        productosDisponibles = JSON.parse(localStorage.getItem("productos"));
-        generarCardsProductos(productosDisponibles);
-
-    }
+    // Seteamos que en productosDisponible filtre el producto con el valor.nombre, hacerlo minuscula y que incluya el target value
+    const productosFilter = productosDisponibles.filter((producto) => producto.nombre.toLowerCase().includes(e.target.value.toLowerCase()));
+    generarCartasProductos(productosFilter);
 
 });
 
-//////////////////////////
-//Filtro por categoria segun pick en lista
 filterLista.addEventListener("click", (e) => {
-    const productosFilter = productosDisponibles.filter((producto) => producto.categorias.toLowerCase().includes(e.target.innerHTML.toLowerCase()));
+    e.preventDefault(); // Evitamos que se ejecute el comportamiento predeterminado del enlace
+    const categoriaSeleccionada = e.target.innerHTML;
+    console.log("Categoría seleccionada:", categoriaSeleccionada); // Verificar que se está capturando la categoría correcta
 
-    //Una vez tengo el nuevo arreglo, sobre escribo para generar la carta
-    productosDisponibles = productosFilter;
+    let productosFiltrados;
 
-    if (e.target.innerHTML !== "Todos") {
-        generarCardsProductos(productosFilter);
+    if (categoriaSeleccionada.toLowerCase() !== "todos") {
+        productosFiltrados = productosDisponibles.filter((producto) => producto.categorias.includes(categoriaSeleccionada));
     } else {
-        productosDisponibles = JSON.parse(localStorage.getItem("productos"));
-        generarCardsProductos(productosDisponibles);
-
+        productosFiltrados = productosDisponibles;
     }
 
+    console.log("Productos filtrados:", productosFiltrados); // Verificar los productos filtrados
+    console.log("Categorías disponibles:", productosDisponibles.map(producto => producto.categorias).flat());
+    generarCartasProductos(productosFiltrados);
 });
+
+
+
+
+//Filtro por categorias segun pick en lista
+// filterLista.addEventListener("click", (e) => {
+//     const categoriaSeleccionada = e.target.innerHTML.toLowerCase();
+//     let productosFiltrados;
+
+//     if (categoriaSeleccionada !== "Todos") {
+//         productosFiltrados = productosDisponibles.filter((producto) => producto.categorias.toLowerCase().includes(categoriaSeleccionada));
+//     } else {
+//         productosFiltrados = productosDisponibles;
+//     }
+
+//     generarCartasProductos(productosFiltrados);
+// });
+
+
+// //////////////////////////
+// // //Filtro por categoria segun pick en lista
+// filterLista.addEventListener("click", (e) => {
+//     const productosFilter = productosDisponibles.filter((producto) => producto.categorias.toLowerCase().includes(e.target.innerHTML.toLowerCase()));
+
+//     //Una vez tengo el nuevo arreglo, sobre escribo para generar la carta
+//     productosDisponibles = productosFilter;
+
+//     if (e.target.innerHTML !== "Todos") {
+//         generarCardsProductos(productosFilter);
+//     }
+
+// });
+
 
 //////////////////////////
 //Filter por Nombre
-filterNombre.addEventListener("click", (e) => {
-    filtrarPorNombre(e.target.innerHTML);
-});
+// filterNombre.addEventListener("click", (e) => {
+//     filtrarPorNombre(e.target.innerHTML);
+// });
 
-//Creamos Funcion
-const filtrarPorNombre = (orden) => {
-    //Creamos esta variable para decirle que copia el orden que le dimos al array
-    let productos;
-    //Utilizamos el sort para que ordene sobre el array existente
-    if (orden === "Nombre (Ascendente)") {
-        productos = productosDisponibles.sort((a, b) => {
-            //Si el nombre por el que empieza tiene un grado mayor que la del otro nombre me devuelve un valor(Bolean) y sigue con el sig.
-            if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
-                return 1;
-            } else if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
-    } else if (orden === "Nombre (Descendente)") {
-        productos = productosDisponibles.sort((a, b) => {
-            if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
-                return 1;
-            } else if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
-                return -1;
-            } else {
-                return 0;
-            }
-        });
-    }
+// //Creamos Funcion
+// const filtrarPorNombre = (orden) => {
+//     //Creamos esta variable para decirle que copia el orden que le dimos al array
+//     let productos;
+//     //Utilizamos el sort para que ordene sobre el array existente
+//     if (orden === "Nombre (Ascendente)") {
+//         productos = productosDisponibles.sort((a, b) => {
+//             //Si el nombre por el que empieza tiene un grado mayor que la del otro nombre me devuelve un valor(Bolean) y sigue con el sig.
+//             if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
+//                 return 1;
+//             } else if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
+//                 return -1;
+//             } else {
+//                 return 0;
+//             }
+//         });
+//     } else if (orden === "Nombre (Descendente)") {
+//         productos = productosDisponibles.sort((a, b) => {
+//             if (a.nombre.toLowerCase() < b.nombre.toLowerCase()) {
+//                 return 1;
+//             } else if (a.nombre.toLowerCase() > b.nombre.toLowerCase()) {
+//                 return -1;
+//             } else {
+//                 return 0;
+//             }
+//         });
+//     }
 
-    generarCardsProductos(productos);
-}
+//     generarCardsProductos(productos);
+// }
 
 //////////////////////////
 //Filtro por precio
 
-filterPrecio.addEventListener("click", (e) => {
-    const orden = e.target.innerHTML;
-    let productos;
-    if (orden === "Precio (Menor a Mayor)") {
-        productos = productosDisponibles.sort((a, b) => a.precio - b.precio);
-    } else if (orden === "Precio (Mayor a Menor)") {
-        productos = productosDisponibles.sort((a, b) => b.precio - a.precio);
-    }
+// filterPrecio.addEventListener("click", (e) => {
+//     const orden = e.target.innerHTML;
+//     let productos;
+//     if (orden === "Precio (Menor a Mayor)") {
+//         productos = productosDisponibles.sort((a, b) => a.precio - b.precio);
+//     } else if (orden === "Precio (Mayor a Menor)") {
+//         productos = productosDisponibles.sort((a, b) => b.precio - a.precio);
+//     }
 
-    generarCardsProductos(productos);
-})
+//     generarCardsProductos(productos);
+// })
